98ab16b3452efbf8f204a9740e7d916a1d4d5a5b
dewsara-intrepid <Dewsara.Wijerathne@intrepidtravel.com>
2025-03-12 13:47:23 +0530

    Added Utils

--- app/components/LayerComponent.vue
+++ app/components/LayerComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <ClientOnly > -->
   <div style="border:4px solid rgba(230, 223, 37); border-radius: 20px;background-color: rgba(230, 223, 37,0.2); padding : 20px; margin-bottom: 10px;">
-    <div>Layer App {{ '<LayerComponent />'}} :- Get Post Comments</div>
+    <div>Layer App {{ '<LayerComponent />'}} :- Get Post Comments {{ formattedDate }}</div>
     <p>{{ dataStore.data?.[0] }}</p>
     <img src="@layer-app/public/img/image.png" alt="Discover Nuxt 3" width="60px" height="60px"/>
   </div>
@@ -9,9 +9,13 @@
 </template>
 
 <script setup lang="ts">
-import { useDummyDataStore } from '~/stores/useDummyDataStore';
+import { useDummyDataStore } from '@layer-app/app/stores/useDummyDataStore';
+import { formatDate } from '@layer-app/app/utils/formates';
 
 const dataStore = useDummyDataStore();
+
+const myDate = new Date(); // Current date
+const formattedDate = formatDate(myDate, 'DD/MM/YYYY');
 
 onMounted(async () => {
   //<!-- !This is to call the 3rd Party API through Apps server
